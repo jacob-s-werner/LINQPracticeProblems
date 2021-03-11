8c2ed6e4e5c41bd048089b520978575d4bf7471e
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-11 10:57:07 -0600

    In RunProblem1 of LinqProblems Class; Created a var thStringList that searchs through words list for matching words that contain 'th', then adds them to thStringList, and foreach loops through to add them to newList, which is returned.

--- PracticeProblemsLINQ/LinqProblems.cs
+++ PracticeProblemsLINQ/LinqProblems.cs
@@ -17,75 +17,82 @@ namespace PracticeProblemsLINQ
         public static List<string> RunProblem1(List<string> words)
         {
             //code
+            var thStringList = words.Where(word => word.Contains("th")).Select(word => word);
+            List<string> newList = new List<string>();
 
-            //return
+            foreach (string word in thStringList)
+            {
+                newList.Add(word);
+            }
             
-        }
-        #endregion
-
-        #region Problem 2 
-        //(5 points) Problem 2
-        //Using LINQ, write a method that takes in a list of strings and returns a copy of the list without duplicates.
-        public static List<string> RunProblem2(List<string> names)
-        {
-            //code
-
             //return
-
+            return newList;
         }
         #endregion
 
-        #region Problem 3
-        //(5 points) Problem 3
-        //Using LINQ, write a method that takes in a list of customers and returns the lone customer who has the name of Mike. 
-        public static Customer RunProblem3(List<Customer> customers)
-        {
-            //code
+        //#region Problem 2 
+        ////(5 points) Problem 2
+        ////Using LINQ, write a method that takes in a list of strings and returns a copy of the list without duplicates.
+        //public static List<string> RunProblem2(List<string> names)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Problem 4
-        //(5 points) Problem 4
-        //Using LINQ, write a method that takes in a list of customers and returns the customer who has an id of 3. 
-        //Then, update that customer's first name and last name to completely different names and return the newly updated customer from the method.
-        public static Customer RunProblem4(List<Customer> customers)
-        {
-            //code
+        //#region Problem 3
+        ////(5 points) Problem 3
+        ////Using LINQ, write a method that takes in a list of customers and returns the lone customer who has the name of Mike. 
+        //public static Customer RunProblem3(List<Customer> customers)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Problem 5
-        //(5 points) Problem 5
-        //Using LINQ, write a method that calculates the class grade average after dropping the lowest grade for each student.
-        //The method should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), 
-        //drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
-        //Expected output: 86.125
-        public static double RunProblem5(List<string> classGrades)
-        {
-            //code
+        //#region Problem 4
+        ////(5 points) Problem 4
+        ////Using LINQ, write a method that takes in a list of customers and returns the customer who has an id of 3. 
+        ////Then, update that customer's first name and last name to completely different names and return the newly updated customer from the method.
+        //public static Customer RunProblem4(List<Customer> customers)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Bonus Problem 1
-        //(5 points) Bonus Problem 1
-        //Write a method that takes in a string of letters(i.e. “Terrill”) 
-        //and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
-        public static string RunBonusProblem1(string word)
-        {
-            //code
+        //#region Problem 5
+        ////(5 points) Problem 5
+        ////Using LINQ, write a method that calculates the class grade average after dropping the lowest grade for each student.
+        ////The method should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), 
+        ////drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+        ////Expected output: 86.125
+        //public static double RunProblem5(List<string> classGrades)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
+
+        //#region Bonus Problem 1
+        ////(5 points) Bonus Problem 1
+        ////Write a method that takes in a string of letters(i.e. “Terrill”) 
+        ////and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
+        //public static string RunBonusProblem1(string word)
+        //{
+        //    //code
+
+        //    //return
+
+        //}
+        //#endregion
     }
 }
